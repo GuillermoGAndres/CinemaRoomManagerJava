41fa8fdd2fe12bbcc0559d93b328b15ab20c7231
GuillermoLenovo <gerardobec35@gmail.com>
2020-11-06 21:10:17 -0600

    Refactoring code stage1 arrangement

--- cinema/Cinema.java
+++ cinema/Cinema.java
@@ -30,20 +30,16 @@ public class Cinema {
     public static void printSeats(char[][] theathe, int row, int colum) {
 	System.out.println("Cinema:");
 	// First row numbers
-	for (int i = 0; i < 9; i++) {
-	    if(i == 0)
-		System.out.print(" " + " ");
-	    else
-		System.out.printf("%d ", i);
+	System.out.print(" ");
+	for (int i = 1; i <= 8; i++) {
+	    System.out.printf(" %d", i);
 	}
 	System.out.println();
 	// Second part scenenary
 	for (int i = 0; i < row; i++) {
-	    for (int j = 0; j < colum + 1; j++) {
-		if(j == 0)
-		    System.out.printf("%d ", i + 1);
-		else
-		    System.out.print(theathe[i][j-1] + " ");
+	    System.out.printf("%d", i + 1);
+	    for (int j = 0; j < colum; j++) {
+		System.out.print(" " + theathe[i][j]);
 	    }
 	    System.out.println("");
 	}
